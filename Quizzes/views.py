4bf0fd091ec5d4edf8e0d4414a646891d36e639b
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-07-07 00:02:13 -0700

    worked on course-quiz.html et al

--- Quizzes/views.py
+++ Quizzes/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 from .models import Question, Choice, Course
 
-# Create your views here.
-
 def questions(request):
     questions = Question.objects.all()
     choices = Choice.objects.all()
@@ -37,13 +35,13 @@ def course_quiz(request, course_title):
     current_user = request.user
     course = Course.objects.get(title=course_title) # get the course from the course_id
     question = course.question_set.all() # get the questions through course
-    options = question.choice_set.all() # get the options for the question
+    options = Choice.objects.all() # get the options for the question
 
-    user_choice = request.POST.get('choice')
-    print(user_choice)
+    # user_choice = request.POST.get('choice')
+    # print(user_choice)
 
     context = {
         'user': current_user, 'course': course, 
-        'question': question, 
+        'question': question, 'options':options
     }
     return render(request, 'Quiz/course-quiz.html', context)
